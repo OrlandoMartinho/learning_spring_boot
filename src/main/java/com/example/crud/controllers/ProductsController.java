23382af3699eb2e23cb9381e4aa8ad4cde09eaf6
OrlandoMartinho <srsaiombo@gmail.com>
2025-05-10 19:49:07 +0100

    updating any bug

--- src/main/java/com/example/crud/controllers/ProductsController.java
+++ src/main/java/com/example/crud/controllers/ProductsController.java
@@ -1,18 +1,26 @@
 package com.example.crud.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crud.domain.products.ProductsRepository;
 @RestController
 @RequestMapping("/products")
 
 public class ProductsController {
 
+
+    @Autowired
+    private ProductsRepository productsRepository;
     @GetMapping
 
     public ResponseEntity getAllProduct(){
-        return ResponseEntity.ok("deu Ok");
+
+        var products = productsRepository.findAll();
+        return ResponseEntity.ok(products);
     }
 
 }
