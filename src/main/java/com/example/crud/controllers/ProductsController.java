1b2815c531c3d71e094eb77ac2a1b54ace0cf96f
OrlandoMartinho <srsaiombo@gmail.com>
2025-05-12 22:49:07 +0100

    fiinishing

--- src/main/java/com/example/crud/controllers/ProductsController.java
+++ src/main/java/com/example/crud/controllers/ProductsController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,5 +58,15 @@ public class ProductsController {
         productRepository.save(productToUpdate);
         return ResponseEntity.ok(productToUpdate);
       
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        productRepository.delete(product.get());
+        return ResponseEntity.noContent().build();
     }
 }
